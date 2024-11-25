59c2c5af799328f91d5e875680eadf63559f583d
alTashkov <alextashkov@gmail.com>
2024-11-25 18:58:59 +0200

    Fixed a missing link

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -4,9 +4,10 @@ import { Avatar } from "@/components/ui/avatar";
 import useGetUserProfileById from "../../hooks/useGetUserProfileById";
 import { timeAgo } from "../../utils/timeAgo";
 import useFollowUser from "../../hooks/useFollowUser";
+import { Link } from "react-router-dom";
 const PostHeader = ({ post }) => {
-  const {userProfile} = useGetUserProfileById(post.createdBy);
-  const {isFollowing, handleFollowUser} = useFollowUser(post.createdBy);
+  const { userProfile } = useGetUserProfileById(post.createdBy);
+  const { isFollowing, handleFollowUser } = useFollowUser(post.createdBy);
   return (
     <Flex
       justifyContent={"space-between"}
@@ -15,13 +16,15 @@ const PostHeader = ({ post }) => {
       justifyItems={"center"}
     >
       <Flex alignItems={"center"} gap={2} marginBottom={4}>
-        <Avatar
-          src={userProfile?.profilePicURL}
-          h={{ base: "30px", md: "40px" }}
-          w={{ base: "30px", md: "40px" }}
-        />
+        <Link to={`/${userProfile?.username}`}>
+          <Avatar
+            src={userProfile?.profilePicURL}
+            h={{ base: "30px", md: "40px" }}
+            w={{ base: "30px", md: "40px" }}
+          />
+        </Link>
         <Flex fontSize={12} fontWeight={"bold"} gap={2}>
-          {userProfile?.username}
+          <Link to={`/${userProfile?.username}`}>{userProfile?.username}</Link>
           <Box color={"gray.400"} fontSize={{ base: 10, md: 13 }}>
             {timeAgo(post.createdAt)}
           </Box>
@@ -36,7 +39,7 @@ const PostHeader = ({ post }) => {
           _hover={{ color: "white" }}
           transition={"0.2s ease-in-out"}
         >
-          {isFollowing?"Unfollow":"Follow"}
+          {isFollowing ? "Unfollow" : "Follow"}
         </Text>
       </Box>
     </Flex>
